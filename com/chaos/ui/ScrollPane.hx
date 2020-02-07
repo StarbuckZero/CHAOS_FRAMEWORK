6e7bdc29a76733440ec02b69d3d2a1dc37bcb44b
StarbuckZero <starbuckzero@gmail.com>
2020-02-07 00:27:38 -0500

    Fix error message
    My dumb ass pushed a change without testing first.

--- com/chaos/ui/ScrollPane.hx
+++ com/chaos/ui/ScrollPane.hx
@@ -436,7 +436,7 @@ class ScrollPane extends BaseContainer implements IScrollPane implements IBaseCo
 	override public function draw():Void {
 
 		if(_useCustomRender && UIBitmapManager.hasCustomRenderTexture(ScrollPane.TYPE) && _width > 0 && _height > 0)  {
-			setBackgroundImage(UIBitmapManager.runCustomRender(ScrollPane.TYPE,{"width":_width,"height":_height}););
+			setBackgroundImage(UIBitmapManager.runCustomRender(ScrollPane.TYPE,{"width":_width,"height":_height}));
 		}		
 
 		// Draw background image
